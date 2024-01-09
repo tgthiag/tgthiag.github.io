b69592aa3e9828ef71c2c834ea6903031543b53c
tgthiag <tgthiag@gmail.com>
2024-01-09 00:17:34 -0300

    scratch, in development

---
+++ src/pages/main.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+
+const Perfil = () => {
+  return (
+    <div>
+      <div>
+        <h1>Thiago Carvalho</h1>
+        <p>Analista Desenvolvedor | Web I Mobile | Javascript | Flutter | Android | Kotlin</p>
+        <p>Localização: Guarulhos, São Paulo, Brasil</p>
+        <p>Contato: <a href="mailto:tgthiag@gmail.com">tgthiag@gmail.com</a></p>
+      </div>
+
+      <div>
+        <h2>Objetivo Profissional</h2>
+        <p>Buscando emprego nos seguintes cargos: Desenvolvedor, Desenvolvedor Javascript, Desenvolvedor para Android e Analista de desenvolvimento</p>
+      </div>
+
+      <div>
+        <h2>Sobre Mim</h2>
+        <p>
+          Desenvolvedor na Easy Analytics. Bacharel em Relações Internacionais, MBE em Engenharia da Computação, Pós-graduando em Programação FullStack e Desenvolvimento em Linguagens Eficientes na FIA.
+          Experiência em desenvolvimento para a indústria 4.0: Python & SQL para programação em HMI SCADA, criação de RPA, integrações com ERP TOTVS e SAP, e automatização de relatórios.
+        </p>
+        <p>
+          Internacionalização de aplicações (i18n), melhoria de SEO, desenvolvimento Web utilizando HTML, CSS, Bootstrap & Jquery; desenvolvimento mobile em: Kotlin, React Native e Flutter.
+          Aplicativo publicado na Amazon e Google Play Store; versionamento: GitHub e GitLab; bancos de dados SQL e Google Firebase; API REST com Node express e Python/Flask; Desenvolvimento ágil com Jira, Trello, etc.
+        </p>
+        <p>Inglês avançado, Espanhol Intermediário e Alemão básico.</p>
+        <p>Github: <a href="https://github.com/tgthiag">https://github.com/tgthiag</a></p>
+      </div>
+
+      <div>
+        <h2>Principais Competências</h2>
+        <p>JavaScript • Python • Desenvolvimento de software • Desenvolvimento Android</p>
+      </div>
+
+      <div>
+        <h2>Experiência Profissional</h2>
+        <p>Easy Analytics - Desenvolvedor (Atual)</p>
+        <p>Empresa XYZ - Desenvolvedor FullStack (Anterior)</p>
+        {/* Add more experiences as needed */}
+      </div>
+
+      <div>
+        <h2>Educação</h2>
+        <p>MBE em Engenharia da Computação - Universidade ABC</p>
+        <p>Bacharel em Relações Internacionais - Universidade XYZ</p>
+        <p>Pós-graduação em Programação FullStack - FIA</p>
+        {/* Add more education details as needed */}
+      </div>
+
+      <div>
+        <h2>Projetos</h2>
+        <p>Desenvolvimento do sistema de automação industrial para cliente ABC</p>
+        <p>Publicação do aplicativo móvel XYZ na Google Play Store</p>
+        {/* Add more project details as needed */}
+      </div>
+
+      <div>
+        <h2>Licenças e Certificados</h2>
+        <p>Certificado em Desenvolvimento Android pela Google</p>
+        <p>Licença em Python pela Python Software Foundation</p>
+        {/* Add more license and certificate details as needed */}
+      </div>
+
+      <div>
+        <h2>Recomendações</h2>
+        <p>"Thiago é um profissional dedicado e altamente competente. Seu trabalho no projeto XYZ foi excepcional."</p>
+        {/* Add more recommendations as needed */}
+      </div>
+
+      <div>
+        <h2>Cursos</h2>
+        <p>Curso Avançado de React na Udemy</p>
+        <p>Curso de Desenvolvimento Mobile com Flutter na Pluralsight</p>
+        {/* Add more course details as needed */}
+      </div>
+
+      <div>
+        <h2>Idiomas</h2>
+        <p>Inglês: Avançado</p>
+        <p>Espanhol: Intermediário</p>
+        <p>Alemão: Básico</p>
+        {/* Add more language details as needed */}
+      </div>
+
+      <div>
+        <h2>Interesses</h2>
+        <p>Tecnologia, Desenvolvimento de Software, Inovação, Viagens</p>
+        {/* Add more interests as needed */}
+      </div>
+    </div>
+  );
+}
+
+export default Perfil;
