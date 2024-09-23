d66b1b33374a8f4ee56094ca929b67a48d1a1069
tgthiag02 <thiago.carvalho@zonaazulbrasil.com.br>
2024-09-23 09:57:51 -0300

    Updates

--- dinamica-custom.js
+++ dinamica-custom.js
@@ -227,7 +227,7 @@ function insertData() {
 
                         $produtosRow.on("click", function () {
                             if (produto.QtdAtendida >= produto.QtdSolicitada) {
-                                showAlert("Não é possível adicionar esta quantidade");
+                                showAlert("A quantidade solicitada deste produto já foi atendida.");
                             }
                             $("#codigo").val(produto.DescProduto);
                             $("#codigo").data("valor", produto.ValorUnitario.toFixed(2).toString().replace(/\./g, ",").replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1."));
