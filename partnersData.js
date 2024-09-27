460462b66c2ac89b19ab2428897e915fd75570ba
tgthiag02 <thiago.carvalho@zonaazulbrasil.com.br>
2024-09-27 10:04:29 -0300

    Updates

--- partnersData.js
+++ partnersData.js
@@ -20,14 +20,14 @@
             }
     },
     "whatsapp": {
-        {
+        "1":{
             "name": "leaning languages",
             "description": "Follow us, send us a message.",
             "url": "https://chat.whatsapp.com/KKO8PXzO3wx20ZxfpwNTto",
             "language": "all",
             "learning": "all"
         },
-        {
+        "2":{
             "name": "teste whatsapp",
             "description": "Follow us, send us a message.",
             "url": "https://chat.whatsapp.com/KKO8PXzO3wx20ZxfpwNTto",
@@ -36,14 +36,14 @@
         }
     },
     "telegram":{
-        {
+        "1":{
             "name": "Trump",
             "description": "Follow us, send us a message.",
             "url": "https://tlgrm.eu/channels/politics",
             "language": "all",
             "learning": "all"
         },
-        {
+        "2":{
             "name": "teste telegram",
             "description": "Follow us, send us a message.",
             "url": "https://tlgrm.eu/channels/politics",
