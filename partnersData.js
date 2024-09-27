9d4e35c8b5f73551e87a68ecd6858b139ee7f16e
tgthiag02 <thiago.carvalho@zonaazulbrasil.com.br>
2024-09-27 08:59:04 -0300

    Updates

--- partnersData.js
+++ partnersData.js
@@ -5,5 +5,11 @@
       "description": "For polyglots and language learners.",
       "url": "https://www.facebook.com/groups/polyglotcommunity"
     }
+    "item2": {
+        "image": "https://tgthiag.github.io/images/mylogo.jpg",
+        "name": "Any Language (Facebook Page)",
+        "description": "Follow us, send us a message.",
+        "url": "https://www.facebook.com/profile.php?id=61565091697441"
+      }
   }
   
