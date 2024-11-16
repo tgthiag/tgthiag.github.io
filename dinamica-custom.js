f3d48cab01351b35e6fdc4c92ac21ce556c5d0cc
tgthiag02 <thiago.carvalho@zonaazulbrasil.com.br>
2024-11-15 23:10:34 -0300

    Updates

--- dinamica-custom.js
+++ dinamica-custom.js
@@ -179,9 +179,9 @@ function insertData() {
                                         <h5 class="card-title">${produto.DescProduto}</h5>
                                         <div class="row">
                                             <div class="col-12">
-                                                <p class="card-text d-flex justify-content-between"><span><strong>Cod. Produto:</strong> ${produto.CodigoProduto}</span><span><strong>Item:</strong> ${produto.Item}</span></p>
-                                                <p class="card-text d-flex justify-content-between"><span><strong>Val. Unitario:</strong> ${produto.ValorUnitario}</span><span><strong>Unidade:</strong> ${produto.UnidadeMedida}</span></p>
-                                                <p class="card-text d-flex justify-content-between"><span><strong>Qtd. Disponivel:</strong> ${produto.QtdAtendida}</span><span><strong>Qtd. Solicitada:</strong> ${produto.QtdSolicitada}</span></p>
+                                                <p class="card-text d-flex justify-content-between"><span><strong>Cod. Produto:</strong> ${produto.CodigoProduto}&nbsp;&nbsp;&nbsp;&nbsp;</span><span><strong>Item:</strong> ${produto.Item}</span></p>
+                                                <p class="card-text d-flex justify-content-between"><span><strong>Val. Unitario:</strong> ${produto.ValorUnitario}&nbsp;&nbsp;&nbsp;&nbsp;</span><span><strong>Unidade:</strong> ${produto.UnidadeMedida}</span></p>
+                                                <p class="card-text d-flex justify-content-between"><span><strong>Qtd. Disponivel:</strong> ${produto.QtdAtendida}&nbsp;&nbsp;&nbsp;&nbsp;</span><span><strong>Qtd. Solicitada:</strong> ${produto.QtdSolicitada}</span></p>
                                             </div>
                                         </div>
                                     </div>
